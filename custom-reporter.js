f44936e65cfd3b08a4e3c05cafe75b4eaeaf245d
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2022-12-13 19:04:17 +0530

    Update custom-reporter.js

--- custom-reporter.js
+++ custom-reporter.js
@@ -151,7 +151,7 @@ const writeTextFiles = function (result, outputFiles, cb) {
 
 	var XMLHttpRequest = require('xhr2');
   var xhr = new XMLHttpRequest();
-  var url = "https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw==";
+  var url = "https://yaksha-stage-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=JSssTES1yvRyHXshDwx6m405p0uSwbqnA937NaLAGX7zazwdLPC4jg==";
   xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.onreadystatechange = function () {
